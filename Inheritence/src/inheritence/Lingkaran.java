48b5798f7ee371bafc1cb237a9a2dffb61e0319d
AHMADWILDANSADADHASAN <57095717+AHMADWILDANSADADHASAN@users.noreply.github.com>
2019-11-25 12:42:17 +0700

    Add files via upload

---
+++ Inheritence/src/inheritence/Lingkaran.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritence;
+
+/**
+ *
+ * @author Ahmad Wildan SD
+ */
+class Lingkaran extends bangun_datar {
+float jari;
+float phi=22/7;
+public void luas(){
+Lingkaran ll =new Lingkaran();
+ll.luas=phi*(jari*jari);
+System.out.println("luas Lingkaran : "+ll.luas);
+}
+public void keliling(){
+Lingkaran ll =new Lingkaran();
+ll.keliling=2*phi*jari;
+System.out.println("keliling Lingkaran : "+ll.keliling);
+}
+}
+
+
